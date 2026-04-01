345bd61bdce73b65473d5d701a91fe93b1d3e5d0
kshshton <michkrzyszton@gmail.com>
2026-04-01 11:03:35 +0200

    Improve error handling

--- internal/processor/deserializer.go
+++ internal/processor/deserializer.go
@@ -10,17 +10,26 @@ import (
 )
 
 func Deserializer(msg mqtt.Message) {
-	sensorName := strings.Split(msg.Topic(), "/")[1]
+	var sensorName string
+
+	topicElements := strings.Split(msg.Topic(), "/")
+	if len(topicElements) > 1 {
+		sensorName = topicElements[1]
+	} else {
+		err := fmt.Errorf("Wrong format of topic: %s", msg.Topic())
+		fmt.Println(err.Error())
+	}
 
 	info, ok := sensors.Registry[sensorName]
 	if !ok {
-		fmt.Printf("\nUnknown sensor: %s", msg.Topic())
+		err := fmt.Errorf("\nUnknown sensor: %s", msg.Topic())
+		fmt.Println(err.Error())
 	}
 
 	sensorInstance := info.Factory()
 
 	if err := json.Unmarshal(msg.Payload(), sensorInstance); err != nil {
-		fmt.Println(err)
+		fmt.Println(err.Error())
 	}
 
 	info.Channel <- sensorInstance
