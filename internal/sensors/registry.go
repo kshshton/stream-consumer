e7543f9f7202abfca1bf835bca011d086cfddf89
kshshton <michkrzyszton@gmail.com>
2026-04-01 10:40:40 +0200

    Create schemas folder

--- internal/sensors/registry.go
+++ internal/sensors/registry.go
@@ -1,6 +1,9 @@
 package sensors
 
-import "fmt"
+import (
+	"fmt"
+	schemas "go-server/internal/sensors/schemas"
+)
 
 type SensorInfo struct {
 	Factory func() any
@@ -8,16 +11,16 @@ type SensorInfo struct {
 }
 
 var Registry = map[string]*SensorInfo{
-	"sen0441": {Factory: func() any { return &SEN0441{} }},
-	"scd41":   {Factory: func() any { return &SCD41{} }},
-	"pms5003": {Factory: func() any { return &PMS5003{} }},
-	"ens160":  {Factory: func() any { return &ENS160{} }},
-	"bme280":  {Factory: func() any { return &BME280{} }},
-	"ds18b20": {Factory: func() any { return &DS18B20{} }},
+	"sen0441": {Factory: func() any { return &schemas.SEN0441{} }},
+	"scd41":   {Factory: func() any { return &schemas.SCD41{} }},
+	"pms5003": {Factory: func() any { return &schemas.PMS5003{} }},
+	"ens160":  {Factory: func() any { return &schemas.ENS160{} }},
+	"bme280":  {Factory: func() any { return &schemas.BME280{} }},
+	"ds18b20": {Factory: func() any { return &schemas.DS18B20{} }},
 }
 
 func init() {
-	fmt.Print(Registry)
+	fmt.Print("Called init...")
 	for _, info := range Registry {
 		info.Channel = make(chan any, 10)
 	}
